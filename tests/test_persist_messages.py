605e88dc8ba6e968a31baf2f68e9e564a81f08d8
Andy Hsieh <andy@askmiso.com>
2022-02-03 22:35:40 -0800

    Use Jsonnet template
    We also temporary disable delete. We will enable it once we support ACTIVATE_VERSION

---
+++ tests/test_persist_messages.py
@@ -0,0 +1,32 @@
+""" test persist messages function """
+import json
+from unittest.mock import MagicMock
+
+from target_miso import persist_messages
+
+def test_persist_message():
+    """ Test persist message function is working as expected """
+    dummy_client = MagicMock()
+    dummy_client.write_record = MagicMock()
+    dummy_client.flush = MagicMock()
+    raw_rec = {"asset_id": 123, "asset_title": "title 123"}
+    template = """
+        {
+            # convert asset_id to product_id string
+            product_id: std.toString(data.asset_id),
+            # convert asset_title to simply title 
+            title: data.asset_title
+        }
+    """
+    persist_messages(
+        [json.dumps({"type": "RECORD", "stream": "test_stream", "record": raw_rec})],
+        dummy_client,
+        {"test_stream": template}
+    )
+    dummy_client.write_record.assert_called_once_with(
+        {
+            "product_id": "123",
+            "title": 'title 123'
+        }
+    )
+    dummy_client.flush.assert_called_once_with()
---
+++ tests/test_transform.py
@@ -0,0 +1,32 @@
+""" Test eval jsonnet """
+from target_miso import eval_jsonnet
+
+
+def test_transform():
+    """ Test jsonnet transformation """
+    raw_rec = {"asset_id": 123, "asset_title": "title 123"}
+
+    # basic transform
+    output = eval_jsonnet(
+        """
+          {
+            product_id: std.toString(data.asset_id), 
+            title: data.asset_title
+          }
+        """,
+        raw_rec)
+    assert output == {"product_id": "123", "title": 'title 123'}
+    # merge
+    output = eval_jsonnet(
+        """
+            {
+                product_id: std.toString(data.asset_id), 
+                title: data.asset_title
+    
+            } + {custom_attributes: data}
+        """, raw_rec
+    )
+
+    assert output == {"product_id": "123",
+                      "title": 'title 123',
+                      "custom_attributes": raw_rec}
